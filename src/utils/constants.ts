645e1d87661ee41ec6840020d0fc24d5b9ef44fd
djm10 <djm9826@hotmail.com>
2025-02-23 12:14:25 -0500

    update webhook

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,2 @@
 
-export const MAKE_WEBHOOK_URL = 'https://hook.us2.make.com/fdfea2uux2sa7todteplybdudo45qpwm';
+export const MAKE_WEBHOOK_URL = 'https://hook.us2.make.com/d3kepgsztrxekbdjiwrq8zn05l5zo5bs';
